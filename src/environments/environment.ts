15c6ed3c9fcf40ea56e7319528f3a9a3c5f15542
Harshana Batagalla <67497499+harshanabatagalla@users.noreply.github.com>
2025-06-11 19:05:53 +0530

    fix: update apiUrl to point to the correct Azure region

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://account-balance-viewer-api-bmftachxg5c3ahaa.canadacentral-01.azurewebsites.net/api',
+  apiUrl: 'https://account-balance-viewer-api-bmftachxg5c3ahaa.centralindia-01.azurewebsites.net/api',
   msalConfig: {
     clientId: 'e60c57ff-6f09-4fe3-b5da-ea92a684d8d2', 
     authority: 'https://login.microsoftonline.com/e70219c0-1739-42f3-b579-b8d1475e25cd', 
